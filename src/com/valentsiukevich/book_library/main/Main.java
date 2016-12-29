2fe5a1fa06191e28247b30e3d403e916214b40a7
Ilya <valentsiukevich.ilya@gmail.com>
2016-12-29 17:13:28 +0300

    Test 02

--- src/com/valentsiukevich/book_library/main/Main.java
+++ src/com/valentsiukevich/book_library/main/Main.java
@@ -3,15 +3,21 @@ package com.valentsiukevich.book_library.main;
 import com.valentsiukevich.book_library.book.Library;
 import com.valentsiukevich.book_library.console.Display;
 import com.valentsiukevich.book_library.reader.ReadFile;
-import com.valentsiukevich.book_library.util.Property;
+import com.valentsiukevich.book_library.util.BookProperty;
+import com.valentsiukevich.book_library.util.SortProperty;
 
 public class Main {
     public static void main(String[] args) {
         Library library = new Library();
         library.setBooks(ReadFile.readAllBooks("file.txt"));
         Display.displayLibrary(library);
-        library.find(2012, Property.NAME);
+
+        library.find(2012, BookProperty.YEAR_OF_PUBLISHING);
         System.out.println("ПОИСК...");
+        Display.displayLibrary(library);
+
+        library.sortBy(BookProperty.NAME, SortProperty.ASCENDING);
+        System.out.println("СОРТИРОВКА...");
         Display.displayLibrary(library);
     }
 }
