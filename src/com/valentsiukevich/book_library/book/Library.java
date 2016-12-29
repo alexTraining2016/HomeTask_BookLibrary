e77f0e42cf68fd52ca5da2c9a94a145aa3289f3c
Ilya <valentsiukevich.ilya@gmail.com>
2016-12-29 17:13:04 +0300

    Change method sortBy

--- src/com/valentsiukevich/book_library/book/Library.java
+++ src/com/valentsiukevich/book_library/book/Library.java
@@ -1,8 +1,10 @@
 package com.valentsiukevich.book_library.book;
 
-import com.valentsiukevich.book_library.util.Property;
+import com.valentsiukevich.book_library.sort.*;
+import com.valentsiukevich.book_library.util.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Library {
     // ПОЛЯ
@@ -53,18 +55,35 @@ public class Library {
         books = new ArrayList<>(baseBooks);
     }
 
-    public ArrayList<Book> sortBy(Property property) {
-        return new ArrayList<>();
+    public ArrayList<Book> sortBy(BookProperty bookProperty, SortProperty sortProperty) {
+        switch (sortProperty) {
+            case ASCENDING: {
+                switch (bookProperty) {
+                    case NAME:
+                        Collections.sort(books, new SortByNameAscending());
+                        return books;
+                }
+            }
+            case DESCENDING: {
+                switch (bookProperty) {
+                    case NAME:
+                        Collections.sort(books, new SortByNameDescending());
+                        return books;
+                }
+            }
+            break;
+        }
+        return books;
     }
 
-    public ArrayList<Book> find(int value, Property property) {
-        if (property != Property.YEAR_OF_PUBLISHING) {
+    public ArrayList<Book> find(int value, BookProperty bookProperty) {
+        if (bookProperty != BookProperty.YEAR_OF_PUBLISHING) {
             books = new ArrayList<>();
             return books;
         }
         ArrayList<Book> findBooks = new ArrayList<>();
         for (Book book : books) {
-            if (Integer.valueOf(book.getValue(property).get(0)) == value) {
+            if (Integer.valueOf(book.getValue(bookProperty).get(0)) == value) {
                 findBooks.add(book);
             }
         }
@@ -72,15 +91,15 @@ public class Library {
         return books;
     }
 
-    public ArrayList<Book> find(String value, Property property) {
-        if (property == Property.YEAR_OF_PUBLISHING) {
+    public ArrayList<Book> find(String value, BookProperty bookProperty) {
+        if (bookProperty == BookProperty.YEAR_OF_PUBLISHING) {
             books = new ArrayList<>();
             return books;
         }
         ArrayList<Book> findBooks = new ArrayList<>();
         value = value.toLowerCase();
         for (Book book : books) {
-            if ((book.getValue(property).get(0).toLowerCase()).contains(value)) {
+            if ((book.getValue(bookProperty).get(0).toLowerCase()).contains(value)) {
                 findBooks.add(book);
             }
         }
