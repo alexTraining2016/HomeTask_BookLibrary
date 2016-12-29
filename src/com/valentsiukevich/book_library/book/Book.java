76f1f2a3e025dfcfb7d4bad0f53ca40c78c8339f
Ilya <valentsiukevich.ilya@gmail.com>
2016-12-29 17:11:21 +0300

    Rename Property->BookProperty

--- src/com/valentsiukevich/book_library/book/Book.java
+++ src/com/valentsiukevich/book_library/book/Book.java
@@ -1,6 +1,6 @@
 package com.valentsiukevich.book_library.book;
 
-import com.valentsiukevich.book_library.util.Property;
+import com.valentsiukevich.book_library.util.BookProperty;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Book {
     // ПОЛЯ
     //------------------------------------------------------------------------------------------------------------------
-    private Map<Property, ArrayList<String>> properties;
+    private Map<BookProperty, ArrayList<String>> properties;
     //------------------------------------------------------------------------------------------------------------------
 
     // КОНСТРУКТОРЫ
@@ -33,7 +33,7 @@ public class Book {
             newName = new ArrayList<>();
             newName.add("");
         }
-        properties.put(Property.NAME, new ArrayList<>(Collections.singletonList(newName.get(0))));
+        properties.put(BookProperty.NAME, new ArrayList<>(Collections.singletonList(newName.get(0))));
     }
 
     /**
@@ -46,7 +46,7 @@ public class Book {
             newRating = new ArrayList<>();
             newRating.add("-1");
         }
-        properties.put(Property.RATING, new ArrayList<>(Collections.singletonList(newRating.get(0))));
+        properties.put(BookProperty.RATING, new ArrayList<>(Collections.singletonList(newRating.get(0))));
     }
 
     /**
@@ -59,7 +59,7 @@ public class Book {
             newAuthors = new ArrayList<>();
             newAuthors.add("");
         }
-        properties.put(Property.AUTHOR, newAuthors);
+        properties.put(BookProperty.AUTHOR, newAuthors);
     }
 
     /**
@@ -72,7 +72,7 @@ public class Book {
             newYearOfPublishing = new ArrayList<>();
             newYearOfPublishing.add("-1");
         }
-        properties.put(Property.YEAR_OF_PUBLISHING, new ArrayList<>(Collections.singletonList(newYearOfPublishing.get(0))));
+        properties.put(BookProperty.YEAR_OF_PUBLISHING, new ArrayList<>(Collections.singletonList(newYearOfPublishing.get(0))));
     }
 
     /**
@@ -85,16 +85,16 @@ public class Book {
             newDescription = new ArrayList<>();
             newDescription.add("");
         }
-        properties.put(Property.DESCRIPTION, newDescription);
+        properties.put(BookProperty.DESCRIPTION, newDescription);
     }
 
     private String getStringName() {
-        return properties.get(Property.NAME).get(0);
+        return properties.get(BookProperty.NAME).get(0);
     }
 
     private String getStringRating() {
         String stringRating = "";
-        for (int i = 0; i < Integer.valueOf(properties.get(Property.RATING).get(0)); i++) {
+        for (int i = 0; i < Integer.valueOf(properties.get(BookProperty.RATING).get(0)); i++) {
             stringRating += "✯";
         }
         return stringRating;
@@ -102,26 +102,26 @@ public class Book {
 
     private String getStringAuthors() {
         String stringAuthors = "";
-        for (int i = 0; i < properties.get(Property.AUTHOR).size(); i++) {
-            stringAuthors += properties.get(Property.AUTHOR).get(i) + ", ";
+        for (int i = 0; i < properties.get(BookProperty.AUTHOR).size(); i++) {
+            stringAuthors += properties.get(BookProperty.AUTHOR).get(i) + ", ";
         }
         return stringAuthors;
     }
 
     private String getStringYearOfPublishing() {
-        return properties.get(Property.YEAR_OF_PUBLISHING).get(0);
+        return properties.get(BookProperty.YEAR_OF_PUBLISHING).get(0);
     }
 
     private String getStringDescription() {
-        String stringDescription = properties.get(Property.DESCRIPTION).get(0);
-        for (int i = 1; i < properties.get(Property.DESCRIPTION).size(); i++) {
-            stringDescription += "\n" + properties.get(Property.DESCRIPTION).get(i);
+        String stringDescription = properties.get(BookProperty.DESCRIPTION).get(0);
+        for (int i = 1; i < properties.get(BookProperty.DESCRIPTION).size(); i++) {
+            stringDescription += "\n" + properties.get(BookProperty.DESCRIPTION).get(i);
         }
         return stringDescription;
     }
 
-    ArrayList<String> getValue(Property property) {
-        return properties.get(property);
+    public ArrayList<String> getValue(BookProperty bookProperty) {
+        return properties.get(bookProperty);
     }
 
     @Override
